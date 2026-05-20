834975d13ebffbffedbb93f2f9e82949b908d17b
tonyhaisha <mrcat5535@gmail.com>
2026-05-20 19:06:25 +0300

    small work

--- log-rotate.sh
+++ log-rotate.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
 ARGFILE=$1
+REMOTE_USER=server
+REMOTE_HOST=UbuntuBackups
+
 
 tar -czf $ARGFILE-$(date "+%F").tar.gz $1
+
+scp $ARGFILE-$(date "+%F").tar.gz $REMOTE_USER@$REMOTE_HOST:/logs_rotate/
