51449e815be5fd37a0b6002445de4ec8da0f1fc6
tonyhaisha <mrcat5535@gmail.com>
2026-05-21 12:48:54 +0300

    Expanded main script. TODO: logging and expand args

--- log-rotate.sh
+++ log-rotate.sh
@@ -1,10 +1,19 @@
 #!/bin/bash
 
 ARGFILE=$1
-REMOTE_USER=server
-REMOTE_HOST=UbuntuBackups
+#REMOTE_USER=server
+#REMOTE_HOST=UbuntuBackups
+REMOTE_USER=$2
+REMOTE_HOST=$3
 
-
+echo "Archiving files..."
 tar -czf $ARGFILE-$(date "+%F").tar.gz $1
+echo "Archived successfully!"
 
+echo "Sending files to" $REMOTE_HOST as $REMOTE_USER
 scp $ARGFILE-$(date "+%F").tar.gz $REMOTE_USER@$REMOTE_HOST:/logs_rotate/
+echo "Done!"
+
+echo "Removing source file..."
+rm -r $ARGFILE
+echo "Done!"
