5ee9b4d9932014f87b7babb8adcb4a73060f4ccf
tonyhaisha <mrcat5535@gmail.com>
2026-05-21 17:54:07 +0300

    Added checks for remote server/local directory arguments

--- log-rotate.sh
+++ log-rotate.sh
@@ -3,17 +3,48 @@
 ARGFILE=$1
 #REMOTE_USER=server
 #REMOTE_HOST=UbuntuBackups
-REMOTE_USER=$2
+REMOTE_USER_OR_DIR=$2
 REMOTE_HOST=$3
 
-echo "Archiving files..."
-tar -czf $ARGFILE-$(date "+%F").tar.gz $1
-echo "Archived successfully!"
+if [ $1 ]; then
+	if [ $2 ]; then
+		echo "Archiving files..."
+		tar -czf $ARGFILE-$(date "+%F").tar.gz $1
+		echo "Archived successfully!"
+	else
+	echo "Invalid arguments"
+	exit 1
+	fi
+fi
 
-echo "Sending files to" $REMOTE_HOST as $REMOTE_USER
-scp $ARGFILE-$(date "+%F").tar.gz $REMOTE_USER@$REMOTE_HOST:/logs_rotate/
+if [ $2 ]; then
+	if [ $3 ]; then
+		echo "Sending files to" $REMOTE_HOST as $REMOTE_USER_OR_DIR
+		scp $ARGFILE-$(date "+%F").tar.gz $REMOTE_USER_OR_DIR@$REMOTE_HOST:/logs_rotate/
+	else
+		echo "Saving files in" $REMOTE_USER_OR_DIR
+		if [ -d "$REMOTE_USER_OR_DIR" ]; then
+			cp -rv $ARGFILE-$(date "+%F").tar.gz $REMOTE_USER_OR_DIR
+		else
+			echo "Cannot save files in" $REMOTE_USER_OR_DIR "- directory doesn't exist"
+			exit 1
+		fi
+	fi
+else
+	echo "Invalid arguments"
+	exit 1
+fi
 echo "Done!"
 
-echo "Removing source file..."
-rm -r $ARGFILE
+echo "Do you want to delete source file? (y/any symbol)"
+read SYMBOL
+
+if [ "$SYMBOL" == "y" ]; then
+	echo "Removing source file..."
+	rm -rv $ARGFILE
+else
+	echo "Source file won't be deleted"
+fi
+
 echo "Done!"
+
